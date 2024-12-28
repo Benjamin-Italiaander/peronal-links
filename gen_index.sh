e5d5b53233afcd3db2bed16dd32600e1f4b616e7
Benjamin Italiaander <73101845+Benjamin-Italiaander@users.noreply.github.com>
2024-12-28 16:34:22 +0100

    Update gen_index.sh

--- gen_index.sh
+++ gen_index.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo "# My personl link page" > readme.md
+echo "# My personl link page" > README.md
 
 for f in ./peronal-links.wiki/*; do
 	title="$(cat $f | sed -n '/---/,/---/p'  | grep title: | sed 's/^........//' | sed  's/.$//')"
 	if [ "$title" ]; then
 		description="$(cat $f | sed -n '/---/,/---/p'  | grep description: | sed 's/^..............//' | sed  's/.$//')"
 		url="$(echo $f | sed 's/^...//')"
-		echo "[$title](https://github.com/Benjamin-Italiaander/peronal-links/wiki/$url) $description \n" >> readme.md
+		echo "[$title](https://github.com/Benjamin-Italiaander/peronal-links/wiki/$url) $description \n" >> README.md
 	fi	
 done
 
